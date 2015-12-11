ed8560415c7d16925dce755ddd03f4b299df1459
Tempz <Tempz@users.noreply.github.com>
2015-12-11 19:46:22 +0100

    Added example

---
+++ Agario/AgarioExample/Program.cs
@@ -0,0 +1,95 @@
+﻿/*
+	Copyright (C) 2015 Tempz@users.noreply.github.com
+
+	This file is part of https://github.com/Tempz/Agario
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Linq;
+using Agario;
+
+namespace AgarioExample
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var serverLocations = Game.FetchServerLocations();
+            var serverInfo = serverLocations.First().FetchServerInfo();
+            var partyServer = serverLocations.Skip(1).First().CreateParty();
+            var validParty = Game.VerifyParty(partyServer);
+
+            var connector = serverInfo.CreateSocket();
+            var connector2 = partyServer.CreateSocket();
+            var connector3 = Game.CreateSocket(partyServer.Token);
+
+            Action<World> spawn = world => world.Spawn("AgarioExample");
+            connector.OnReady += world =>
+            {
+                Console.WriteLine("Ready");
+                spawn(world);
+            };
+            connector.OnDied += world =>
+            {
+                Console.WriteLine("Died");
+                spawn(world);
+            };
+            connector.OnUpdate += world => world.MoveTo(0, 0);
+            connector.OnError += exception => Console.WriteLine("Error: " + exception.Message);
+            connector.OnClose += (socketConnector, eventArgs) => Console.WriteLine("Close: " + eventArgs);
+
+            Console.WriteLine("Server locations:");
+            Console.WriteLine(string.Join("\n", serverLocations.Select(location => location.ToString())));
+            Console.WriteLine();
+
+            Console.WriteLine("Server info:");
+            Console.WriteLine(serverInfo);
+            Console.WriteLine();
+
+            Console.WriteLine("Party server:");
+            Console.WriteLine(partyServer);
+            Console.WriteLine();
+
+            Console.WriteLine("Party server from connector 2:");
+            Console.WriteLine(connector2.ServerInfo);
+            Console.WriteLine();
+
+            Console.WriteLine("Party server from connector 3:");
+            Console.WriteLine(connector3.ServerInfo);
+            Console.WriteLine();
+
+            Console.WriteLine("Valid Party:");
+            Console.WriteLine(validParty);
+            Console.WriteLine();
+
+            Console.WriteLine("Connecting to server.");
+            connector.Connect();
+
+            DateTime dateTimeCounter = DateTime.UtcNow;
+            while (connector.IsConnected)
+            {
+                DateTime currentDateTime = DateTime.UtcNow;
+                if (currentDateTime.Subtract(dateTimeCounter).TotalMilliseconds >= 1000)
+                {
+                    dateTimeCounter = currentDateTime;
+
+                    Console.WriteLine(connector.World.MainBall?.ToString() ?? "Player not spawned");
+                }
+
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+    }
+}
--- Agario/AgarioExample/Properties/AssemblyInfo.cs
+++ Agario/AgarioExample/Properties/AssemblyInfo.cs
@@ -1,14 +1,32 @@
-﻿using System.Reflection;
+﻿/*
+	Copyright (C) 2015 Tempz@users.noreply.github.com
+
+	This file is part of https://github.com/Tempz/Agario
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Agario")]
+[assembly: AssemblyTitle("AgarioExample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Agario")]
+[assembly: AssemblyProduct("AgarioExample")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +37,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a7ffcb60-5d57-487e-a908-0a693ebf0ab5")]
+[assembly: Guid("e62235dd-6b97-44d4-88a2-e925a58fb9df")]
 
 // Version information for an assembly consists of the following four values:
 //
